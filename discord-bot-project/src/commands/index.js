ea8939a7359631f625e5667abbf38f7b9bbfdea4
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-17 10:38:11 +0530

    Generated: `discord-bot-project/src/commands/index.js`

---
+++ discord-bot-project/src/commands/index.js
@@ -0,0 +1,17 @@
+```javascript
+const play = require('./play');
+const pause = require('./pause');
+const resume = require('./resume');
+const skip = require('./skip');
+const stop = require('./stop');
+const volume = require('./volume');
+
+module.exports = {
+  play,
+  pause,
+  resume,
+  skip,
+  stop,
+  volume,
+};
+```
